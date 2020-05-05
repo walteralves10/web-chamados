72bf1ec5f44f39f92a10e64f23bc271b230a8dc7
Walter Rezende <walteralves27@gmail.com>
2020-05-05 13:50:20 -0300

    adicionando um campo ao banco

--- default/js/index.js
+++ default/js/index.js
@@ -1,3 +1,26 @@
+var userList = document.getElementById('userList');
 var emailInput = document.getElementById('emailInput');
 var passwordInput = document.getElementById('passwordInput');
 var btnLogin =document.getElementById('btnLogin');
+
+btnLogin.addEventListener('click', function(){
+    create(emailInput.value, passwordInput.value);
+});
+
+function create(email, password){
+    var data = {
+        email: email,
+        password: password
+    };
+
+    return firebase.database().ref().child('users').push(data);
+}
+
+firebase.database().ref('users').on('value', function(snapshot) {
+    userList.innerHTML = '';
+    snapshot.forEach(function(item) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().email + ': ' + item.val().password));
+        userList.appendChild(li);
+    });
+});
