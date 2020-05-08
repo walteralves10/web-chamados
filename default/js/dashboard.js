eb4086131ea3fddcb9f291bff2843b909db02e97
walteralves10 <walteralves27@gmail.com>
2020-05-08 02:23:05 -0300

    teste

--- default/js/dashboard.js
+++ default/js/dashboard.js
@@ -1,3 +1,4 @@
+
 //button
 var btnSair = document.getElementById('btnSair');
 //div
@@ -13,3 +14,13 @@ btnSair.addEventListener('click', function(){
             console.error(error);
         });
 });
+
+/* Listar users do banco atraves de um snapshot
+    firebase.database().ref('person').on('value', function(snapshot) {
+        userList.innerHTML = '';
+        snapshot.forEach(function(item) {
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(item.val().email + ': ' + item.val().name));
+            userList.appendChild(li);
+        });
+    });*/
