89d85846523879c741f5852eeb53fe7a42faa150
Walter Rezende <walteralves27@gmail.com>
2020-05-09 23:03:09 -0300

    add event dashboard button

--- default/js/dashboard.js
+++ default/js/dashboard.js
@@ -20,12 +20,38 @@ btnSair.addEventListener('click', function(){
         });
 });
 
-/* Listar users do banco atraves de um snapshot
-    firebase.database().ref('person').on('value', function(snapshot) {
-        listPendentes.innerHTML = '';
-        snapshot.forEach(function(item) {
-            var li = document.createElement('button');
-            li.appendChild(document.createTextNode(item.val().email + ': ' + item.val().name));
-            userList.appendChild(button);
+function clickItemPendentes(item){
+    //alert(item);
+    //console.log(item);
+    document.getElementById('description').value = item.name;
+}
+
+function renderListPendentes(listsPendentes){
+    listPendentes.innerHTML = '';
+    listsPendentes.forEach(function(item) {
+        var button = document.createElement('button');
+
+        button.addEventListener('click', function(event){
+            clickItemPendentes(JSON.parse(event.target.getAttribute('data')));
         });
-    });*/
+
+        button.setAttribute('class','list-group-item list-group-item-action');
+        button.setAttribute('data', JSON.stringify(item));
+        button.appendChild(document.createTextNode(item.val().email + ' - ' + item.val().name));
+        listPendentes.appendChild(button);
+    });
+}
+
+/* Listar users do banco atraves de um snapshot*/
+    firebase.database().ref('person').on('value', function(snapshot) {
+        //alert('lista');
+        renderListPendentes(snapshot);
+    });
+
+    /*
+            snapshot.forEach(function(item) {
+            
+        });
+            var button = document.createElement('button');
+            button.appendChild(document.createTextNode(item.val().email + ': ' + item.val().name));
+            userList.appendChild(button);*/
