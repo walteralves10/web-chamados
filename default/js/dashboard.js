557bb689588f21079f70e3350d079f821971bd37
walteralves10 <walteralves27@gmail.com>
2020-05-19 02:03:50 -0300

    modification seach urgency

--- default/js/dashboard.js
+++ default/js/dashboard.js
@@ -6,6 +6,37 @@ var listPendentes = document.getElementById('list-pendentes');
 //a
 var listSair = document.getElementById('listSair');
 
+var myLatLng = {lat: 0, lng: 0};
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 17
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+}
+
+function markerMap(){
+    map.innerHTML = '';
+    map = null;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 17
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+}
 
 btnSair.addEventListener('click', function(){
     //alert('teste');
@@ -37,7 +68,10 @@ function clickItemPendentes(item){
     //alert(item);
     //console.log(item);
     document.getElementById('description').value = item.description;
-    //document.getElementById('status').value = item.status;
+    
+    searchPerson(item.personId);
+
+    document.getElementById('status').value = item.status;
     /**
      * latitude
      * longitude
@@ -55,14 +89,29 @@ function renderListPendentes(listsPendentes){
 
         button.setAttribute('class','list-group-item list-group-item-action');
         button.setAttribute('data', JSON.stringify(item));
-        button.appendChild(document.createTextNode(item.val().description /*+ ' - ' + item.val().infoId*/));
+        button.appendChild(document.createTextNode(item.val().description));
         listPendentes.appendChild(button);
     });
 }
 
-/* Listar urgencys do banco atraves de um snapshot*/
-    firebase.database().ref('person/'+'T0ZdsAxOlaNJVn4e65qpjUL0Zkf1').child('urgency').on('value', function(snapshot) {
+function searchPerson(idPerson){
+    firebase.database().ref('person/' + idPerson)/*.child('urgency')*/.on('value', function(snapshot) {
         //alert('lista');
+        addLatLog(JSON.parse(JSON.stringify(snapshot)));
+    });
+}
+
+function addLatLog(person){
+    myLatLng = {
+        lat: Number(person.latitude), 
+        lng: Number(person.longitude)
+    };
+
+    initMap();
+}
+
+/* Listar urgencys do banco atraves de um snapshot*/
+    firebase.database().ref('urgency/').on('value', function(snapshot) {
         renderListPendentes(snapshot);
     });
 
